b019c751edfef11893b9b03a401672ca7a91de79
Fco Javier Fraga Hernández <pachi030919@gmail.com>
2022-11-12 21:21:48 +0100

    Pongo un ejercicio en C

---
+++ P116.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int sumando = 0, cantidad;
+
+    scanf("%d", &cantidad);
+
+    while (sumando < cantidad) {
+        printf("Hola mundo.\n");
+        sumando++;
+    }
+}
