721713323bd73516294edd980989c5ac73ebfafc
Fco Javier Fraga Hernández <pachi030919@gmail.com>
2023-01-04 19:54:58 +0100

    Caso de prueba tipo 3 c++

---
+++ Plantillas/C++/tipo3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void caso_de_prueba(string caso){
+}
+
+int main(){
+    string caso;
+
+    while(cin >> caso){
+        caso_de_prueba(caso);
+    }
+
+    return 0;
+}
